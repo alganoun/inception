01c3b0b2094ecc672f1ee812eaff1f04c0d589b8
Allan Ganoun <alganoun@z1r6p2.42lyon.fr>
2021-11-03 15:17:22 +0100

    finished

--- Linux/srcs/requirements/mariadb/tools/run.sh
+++ Linux/srcs/requirements/mariadb/tools/run.sh
@@ -2,10 +2,12 @@
 if [ ! -d "/tmp/ok" ]; then
 	mysql_install_db
     service mysql start
-    mysql -e "CREATE USER '$DB_USER'@'localhost' identified by '$DB_PASSWORD';" &&\
-	mysql -e "CREATE DATABASE wordpress;" &&\
-	mysql -e "GRANT ALL PRIVILEGES ON *.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';" &&\
-	mysql -e "FLUSH PRIVILEGES;"
+    mysql -u root -e "ALTER USER root@localhost IDENTIFIED WITH mysql_native_password;"
+	mysql -u root -e "ALTER USER root@localhost IDENTIFIED BY '$ROOT_PASSWORD';"
+	mysql -u root -p${ROOT_PASSWORD} -e "CREATE USER '$DB_USER'@'localhost' identified by '$DB_PASSWORD';" &&\
+	mysql -u root -p${ROOT_PASSWORD} -e "CREATE DATABASE wordpress;" &&\
+	mysql -u root -p${ROOT_PASSWORD} -e "GRANT ALL PRIVILEGES ON *.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';" &&\
+	mysql -u root -p${ROOT_PASSWORD} -e "FLUSH PRIVILEGES;"
 	mkdir /tmp/ok
 
 fi
